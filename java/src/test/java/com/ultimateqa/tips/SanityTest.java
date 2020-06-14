f210c7e7ff9cd9724271ceccec604906518e8a4b
nadvolod <nadvolod@gmail.com>
2020-06-14 18:47:18 -0400

    add 1st java test

---
+++ java/src/test/java/com/ultimateqa/tips/SanityTest.java
@@ -0,0 +1,13 @@
+package com.ultimateqa.tips;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SanityTest {
+    @Test
+    public void shouldPass()
+    {
+        assertTrue(true);
+    }
+}
